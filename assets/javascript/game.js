dd08ccdcbde664684d86d83485e0f3cf4415b51f
Jessica <JCOJOHNS2@GMAIL.COM>
2019-06-26 11:24:56 -0500

    finished theme and commented code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,13 @@
 var validGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wordList = ["chocobo", "materia", "bahamut", "crystal", "summoner", "ifrit", "shiva", "cure", "dragoon", "cid", "biggs", "wedge", ]
-var spaceanswerList = []
 var answerList = [];
 var UsedLetter = [];
 var userAttempts = 7;
 var gameOver = false;
 var Score = 0;
-var Win = false; //adding wins error
 
 
+//pressing enter to start the game
 
 function start() {
         document.onkeyup = function (event) {
@@ -19,7 +18,11 @@ function start() {
         }
 }
 
+//the game loop
+
 function game() {
+
+    //randomly picking a word from the word list
 
     var computerWord = wordList[Math.floor(Math.random() * wordList.length)];
     console.log(computerWord)
@@ -41,27 +44,32 @@ function game() {
     answerList = dashesIntoworlds.split('');
 
     
-    //removing last item in array from split
+    //removing last item in array from split - no longer needed
     //answerList.pop();
     //console.log(answerList)
 
-    //game loops and conditions
 
+    //pushing score and dashes into html
     document.getElementById("WinResult").innerHTML = Score;
     
     document.getElementById("dashes").innerHTML = answerList.join("  ");
 
-    document.onkeyup = function (event) {
-        
+    //keyboard inputs are placed inside of a variable
 
+    document.onkeyup = function (event) {
         var userGuess = event.key.toLowerCase();
-        //console.log(userGuess)
         
+    //signaling the game to start
 
         if (gameOver === false) {
 
+            //cheaking that the input is a letter of the alphabet
+
             if (validGuess.indexOf(userGuess) > -1) {
                 console.log("valid key " + userGuess + " pressed!")
+
+                //taking input and searching to see if it is in the randomly picked word
+                //if it is in the word, it will replace it with that character
                 
                 if (computerWord.indexOf(userGuess) > -1) {
                     for (var i = 0; i < computerWord.length + 1; i++) {
@@ -71,6 +79,8 @@ function game() {
                         }
                     }
                 }
+
+                //if the letter is not in the word, character is pushed into user guesses variable
                 else {
                     if (UsedLetter.indexOf(userGuess) === -1) {
                         UsedLetter.push(userGuess);
@@ -80,6 +90,8 @@ function game() {
             else {
                 console.log("invalid key pressed!")
             }
+
+            //updates webpage with the used letters and number of tries by user
             
             document.getElementById("UsedLetters").innerHTML = "Used Letters: " + UsedLetter;
             document.getElementById("NumberTries").innerHTML = "Number of tries: " + (userAttempts - UsedLetter.length);
@@ -87,7 +99,8 @@ function game() {
             
         }
 
-        
+            //lost game condition  
+            //brings code back to start to await enter input      
 
         if (UsedLetter.length > userAttempts - 1) {
 
@@ -98,13 +111,17 @@ function game() {
             start()
 
         }
+
+            //win game condition
+            //updates score
+            //brings code back to start to await enter input
+
         if (answerList.indexOf("_") === -1) {
             Score += 1; 
             console.log("You win!")
             document.getElementById("dashes").innerHTML = "You Win! Press enter to Play again!";
             document.getElementById("WinResult").innerHTML = Score;
             gameOver === !false;
-           
             UsedLetter = [];
             start()
             
